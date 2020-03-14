2004dde4651938f50aec4cd7fb435b2948d24e53
Vladislav Ovchinnikov <ovchinnikovva@okta-solutions.com>
2020-03-14 23:10:27 +0300

    Изменил пример вывода из контроллера

--- src/main/java/ml/socshared/template/controller/v1/HelloController.java
+++ src/main/java/ml/socshared/template/controller/v1/HelloController.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
 import ml.socshared.template.api.v1.rest.HelloApi;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping(value = "/api/v1")
 public class HelloController implements HelloApi {
 
     @Override
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String print() {
-        return "Hello, world";
+    public HashMap<String, String> printHelloWorld() {
+        return new HashMap<>() {
+            { put("text", "Hello, World"); }
+        };
     }
 
 }
