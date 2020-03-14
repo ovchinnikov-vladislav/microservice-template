b54c0d9a93a140c84fbaa5e4014217c9eaa63f95
Vladislav Ovchinnikov <ovchinnikovva@okta-solutions.com>
2020-03-15 02:23:35 +0300

    Внесены правки в тест ip

--- src/main/java/ml/socshared/template/controller/v1/HelloController.java
+++ src/main/java/ml/socshared/template/controller/v1/HelloController.java
@@ -1,12 +1,17 @@
 package ml.socshared.template.controller.v1;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.MediaType;
 import ml.socshared.template.api.v1.rest.HelloApi;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/api/v1")
@@ -18,6 +23,11 @@ public class HelloController implements HelloApi {
         return new HashMap<>() {
             { put("text", "Hello, World"); }
         };
+    }
+
+    @GetMapping(value = "/ip", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String ip() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostAddress();
     }
 
 }
