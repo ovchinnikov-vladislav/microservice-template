e2c73161cd170f390135a6b984231dc0e6657118
Vladislav Ovchinnikov <ovchinnikovva@okta-solutions.com>
2020-03-15 03:16:57 +0300

    Добавлено исключение Exception для стандартной обработки исключений

--- src/main/java/ml/socshared/template/handler/RestExceptionHandler.java
+++ src/main/java/ml/socshared/template/handler/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ml.socshared.template.exception.AbstractRestHandleableException;
@@ -23,6 +24,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<RestApiError> handlePrintException(ServletWebRequest webRequest, AbstractRestHandleableException exc) {
         log.error(exc.getMessage());
         return buildErrorResponse(exc, exc.getHttpStatus(), webRequest, exc.getErrorCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestApiError> handlePrintException(ServletWebRequest webRequest, Exception exc) {
+        log.error(exc.getMessage());
+        return buildErrorResponse(exc, HttpStatus.INTERNAL_SERVER_ERROR, webRequest, AswErrors.INTERNAL);
     }
 }
 
